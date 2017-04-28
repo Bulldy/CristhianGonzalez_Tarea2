c31002bbe37e46f6dfbf06fe7486922d8de56b99
Cristhian Gonzalez <ce.gonzalez10@uniandes.edu.co>
2017-04-28 13:46:51 -0500

    sedov_plot.py listo, make clean listo, borre graficas, .txt y .out

--- Makefile
+++ Makefile
@@ -9,4 +9,8 @@ plotshock:
 	python shock_plot.py
 plotsedov:
 	python sedov_plot.py
-
+clean: 
+	rm -f density.txt pressure.txt velocity.txt
+	rm -f r10.txt r60.txt r120.txt t10.txt t60.txt t120.txt
+	rm -f sedov.out
+	rm -f shock.out
--- sedov_plot.py
+++ sedov_plot.py
@@ -2,3 +2,38 @@ import numpy as np
 import matplotlib
 matplotlib.use('Agg')
 import matplotlib.pyplot as plt
+
+rho10=np.genfromtxt("r10.txt")
+rho60=np.genfromtxt("r60.txt")
+rho120=np.genfromtxt("r120.txt")
+
+t10=np.genfromtxt("t10.txt")
+t60=np.genfromtxt("t60.txt")
+t120=np.genfromtxt("t120.txt")
+
+r=np.linspace(0,128,65)
+
+fig1=plt.figure()
+plt.plot(r,rho10)
+plt.title(r'$t= %f $ ' % t10)
+plt.xlabel(r'$r (m)$')
+plt.ylabel(r'$\rho_{10} / \rho_{atm}$')
+plt.savefig("sedov10.pdf")
+plt.close()
+
+fig1=plt.figure()
+plt.plot(r,rho60)
+plt.title(r'$t= %f $' % t60)
+plt.xlabel(r'$r (m)$')
+plt.ylabel(r'$\rho_{60} / \rho_{atm}$')
+plt.savefig("sedov60.pdf")
+plt.close()
+
+fig1=plt.figure()
+plt.plot(r,rho120)
+plt.title(r'$t= %f $' % t120)
+plt.xlabel(r'$r (m)$')
+plt.ylabel(r'$\rho_{120} / \rho_{atm}$')
+plt.savefig("sedov120.pdf")
+plt.close()
+
